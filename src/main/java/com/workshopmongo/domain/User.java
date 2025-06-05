7694dd2d8176af7c4ae8a7cd975b1d1e508a2015
FranciscoDitutala <ditutalafrancisco99@gmail.com>
2025-06-05 22:32:21 +0100

    Usando padrão DTO para retornar usuários

--- src/main/java/com/workshopmongo/domain/User.java
+++ src/main/java/com/workshopmongo/domain/User.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
-@Document(collection ="user")
+@Document(collection ="users")
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/workshopmongo/resources/UserResource.java
+++ src/main/java/com/workshopmongo/resources/UserResource.java
@@ -2,27 +2,30 @@ package com.workshopmongo.resources;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.workshopmongo.config.dto.UserDTO;
 import com.workshopmongo.domain.User;
 import com.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value ="/user/")
+@RequestMapping(value ="/users/")
 public class UserResource {
 
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto =   list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 
 	}
 
